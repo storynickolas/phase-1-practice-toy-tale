1818e4171a86150e0f5d87292ffa27ddde1f63ec
Nick Story <storynickolasl@gmail.com>
2022-06-07 17:34:37 -0400

    commit tree 83322315682d18958d17398021a5e44c443ecde8

--- src/index.js
+++ src/index.js
@@ -12,4 +12,74 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  const toyAddBttn = document.getElementsByClassName('add-toy-form')
+
+  toyAddBttn[0].addEventListener('submit', test)
+  function test (e) {
+    e.preventDefault()
+    let userAddedToy = {
+      name: document.getElementsByClassName("input-text")[0].value,
+      image: document.getElementsByClassName("input-text")[1].value,
+      likes: 0
+    }
+    addNewToy(userAddedToy)
+  }
+
+
+  function getToys() {
+    fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(toys => toys.forEach(toy => renderToy(toy)))
+  }
+
+  getToys()
+
 });
+
+function renderToy(item) {
+  let newItem = document.createElement('div')
+  newItem.id = item.id
+  newItem.className = 'card'
+  newItem.innerHTML = `
+    <h2>${item.name}</h2>
+    <img src="${item.image}" class="toy-avatar">
+    <p class="likes">${item.likes} Likes</p>
+    <button id=${item.id} class='like-btn'>Like ❤️</button>
+  `
+  newItem.querySelector('.like-btn').addEventListener('click', () => {
+    item.likes++
+    newItem.querySelector('.likes').textContent = `${item.likes} Likes`
+    updateLikes(newItem, item.likes)
+  })
+
+  document.getElementById("toy-collection").append(newItem)
+}
+
+function addNewToy(userAddedToy) {
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body:JSON.stringify(userAddedToy)
+    })
+    .then(res => res.json())
+    .then(data => renderToy(data))
+}
+
+function updateLikes(item, newLikes) {
+  fetch(`http://localhost:3000/toys/${item.id}`, {
+    method: 'PATCH',
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "likes": newLikes
+    })})
+    .then(res => res.json())
+    .then(item => console.log(item))
+}
